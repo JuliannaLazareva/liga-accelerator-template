fa12a95e3e16ec6b04530cd6a5b54bf5815e55b9
JuliannaLazareva <86108150+JuliannaLazareva@users.noreply.github.com>
2022-07-26 19:48:51 +0300

    исправила main.js

--- source/js/main.js
+++ source/js/main.js
@@ -3,24 +3,24 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-navMain.classList.remove('main-nav--no-js');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
+
+  const navMain = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
 
   // Utils
   // ---------------------------------
