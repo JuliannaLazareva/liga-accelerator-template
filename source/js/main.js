f1063a80c3f887bda892b6bf4c13747e7b9951e3
JuliannaLazareva <86108150+JuliannaLazareva@users.noreply.github.com>
2022-07-26 19:07:19 +0300

    исправления багов

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,21 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--no-js');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
